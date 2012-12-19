8fd8224cb328936f001ff79c1f2b48cec42d1631
James Lee <jlee@thestaticvoid.com>
2012-12-19 09:49:24 -0500

    Enable loading message for all AJAX calls

--- static/custom.js
+++ static/custom.js
@@ -7,3 +7,23 @@ $(document).ready(function() {
         ui.prevPage.remove();
     });
 });
+
+/*
+ * Enable page loading notifications for Nitrogen calls
+ * (ajax actoins performed outside of jQuery Mobile)
+ *
+ * See: http://jquerymobile.com/demos/1.2.0/docs/pages/loader.html
+ *      http://stackoverflow.com/questions/7208609/show-page-loading-spinner-on-ajax-call-in-jquery-mobile
+ */
+var timer;
+
+$(document).ajaxStart(function() {
+    timer = setTimeout(function() {
+        $.mobile.loading('show');
+    }, 50);
+});
+
+$(document).ajaxStop(function() {
+    clearTimeout(timer);
+    $.mobile.loading('hide');
+});
