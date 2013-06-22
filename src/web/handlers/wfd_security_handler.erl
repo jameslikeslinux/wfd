559d6f24830bb1a8f10a9d988594d3784a67d89a
James Lee <jlee@thestaticvoid.com>
2013-06-22 00:07:44 -0400

    wfd_security_handler: Add debug statements

--- src/web/handlers/wfd_security_handler.erl
+++ src/web/handlers/wfd_security_handler.erl
@@ -20,25 +20,32 @@
 -behaviour(security_handler).
 -export([init/2, finish/2]).
 
+-include_lib("nitrogen_core/include/wf.hrl").
 -include("wfd.hrl").
 
 init(_Config, State) -> 
     case wf:user() of
         undefined ->
+            wf:info("~p: Unknown user", [?MODULE]),
             case string:tokens(wf:cookie(remember_me_token), ":") of
                 [Series, Token] ->
+                    wf:info("~p: Got remember-me cookie: ~p ~p", [?MODULE, Series, Token]),
                     case wfd_user_server:remember_me_login(Series, Token) of
                         {ok, User, Roles, {Series, NewToken, _LastUsed}} ->
+                            wf:info("~p: Used cookie to log in as ~p; setting new cookie: ~p ~p", [?MODULE, User, Series, NewToken]),
                             wf:user(User),
                             lists:foreach(fun(Role) -> wf:role(Role, true) end, Roles),
                             wf:cookie(remember_me_token, Series ++ ":" ++ NewToken, "/", ?REMEMBER_ME_TTL);
                         {error, bad_series} ->
+                            wf:info("~p: Cookie had a bad series; deleting cookie", [?MODULE]),
                             wf:cookie(remember_me_token, "", "/", 0);
                         {error, bad_token} ->  % theft assumed
+                            wf:info("~p: Cookie had a bad token; theft assumed; deleting cookie", [?MODULE]),
                             wf:cookie(remember_me_token, "", "/", 0),
                             wf_context:page_module(wfd_cookie_theft)                                                       
                     end;
-                _ ->
+                BadCookie ->
+                    wf:info("~p: Got bad cookie: ~p", [?MODULE, BadCookie]),
                     ok  % invalid cookie, but it's not worth erasing
             end; 
         _User ->
