96bc972dd8b8a15ffdd09c4cbff06fc6be045422
James Lee <jlee@thestaticvoid.com>
2013-06-25 01:37:05 -0400

    Revert "Revert "Revert "Test: Going overboard with a 30 second AJAX timeout"""
    This reverts commit a10e9cc6865b8f47f6c84e327bd04e75e0703ea5.

--- test/web_SUITE_data/spec/spec_helper.rb
+++ test/web_SUITE_data/spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'capybara-screenshot/rspec'
 
 Capybara.default_driver = :selenium
 Capybara.app_host = 'http://localhost:8082'
-Capybara.default_wait_time = 30
+Capybara.default_wait_time = 5
 
 def login_pending_user
     fill_in 'Username', :with => 'pendinguser'
