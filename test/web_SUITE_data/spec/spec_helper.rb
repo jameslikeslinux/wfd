bb75811641c0ee840940278fd35ca27285231539
James Lee <jlee@thestaticvoid.com>
2013-06-25 01:40:47 -0400

    Test: Try click_button for log in button

--- test/web_SUITE_data/spec/spec_helper.rb
+++ test/web_SUITE_data/spec/spec_helper.rb
@@ -9,11 +9,11 @@ Capybara.default_wait_time = 5
 def login_pending_user
     fill_in 'Username', :with => 'pendinguser'
     fill_in 'Password', :with => 'password'
-    click_on 'Login'
+    click_button 'Login'
 end
 
 def login_valid_user
     fill_in 'Username', :with => 'validuser1'
     fill_in 'Password', :with => 'password'
-    click_on 'Login'
+    click_button 'Login'
 end
