aa846d2cb21ed9169e92c7977244c9eba239f06a
James Lee <jlee@thestaticvoid.com>
2013-06-25 01:47:33 -0400

    Revert "Test: Try click_button for log in button"
    This reverts commit bb75811641c0ee840940278fd35ca27285231539.

--- test/web_SUITE_data/spec/spec_helper.rb
+++ test/web_SUITE_data/spec/spec_helper.rb
@@ -9,11 +9,11 @@ Capybara.default_wait_time = 5
 def login_pending_user
     fill_in 'Username', :with => 'pendinguser'
     fill_in 'Password', :with => 'password'
-    click_button 'Login'
+    click_on 'Login'
 end
 
 def login_valid_user
     fill_in 'Username', :with => 'validuser1'
     fill_in 'Password', :with => 'password'
-    click_button 'Login'
+    click_on 'Login'
 end
